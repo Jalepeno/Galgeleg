772098ef47f3eaaf4c211b5f5f61762dc4aa8625
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2013-08-26 09:52:36 +0200

    1

---
+++ src/galgeleg/Galgelogik.java
@@ -0,0 +1,155 @@
+package galgeleg;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+
+public class Galgelogik {
+  private ArrayList<String> muligeOrd  = new ArrayList<String>();
+  private String ordet;
+  private ArrayList<String> brugteBogstaver = new ArrayList<String>();
+  private String synligtOrd;
+  private int antalForkerteBogstaver;
+  private boolean sidsteBogstavVarKorrekt;
+  private boolean spilletErVundet;
+  private boolean spilletErTabt;
+
+
+  public ArrayList<String> getBrugteBogstaver() {
+    return brugteBogstaver;
+  }
+
+  public String getSynligtOrd() {
+    return synligtOrd;
+  }
+
+  public String getOrdet() {
+    return ordet;
+  }
+
+  public int getAntalForkerteBogstaver() {
+    return antalForkerteBogstaver;
+  }
+
+  public boolean erSidsteBogstavKorrekt() {
+    return sidsteBogstavVarKorrekt;
+  }
+
+  public boolean erSpilletVundet() {
+    return spilletErVundet;
+  }
+
+  public boolean erSpilletTabt() {
+    return spilletErTabt;
+  }
+
+  public boolean erSpilletSlut() {
+    return spilletErTabt || spilletErVundet;
+  }
+
+
+  public Galgelogik() {
+    muligeOrd.add("bil");
+    muligeOrd.add("computer");
+    muligeOrd.add("programmering");
+    muligeOrd.add("motorvej");
+    muligeOrd.add("busrute");
+    muligeOrd.add("gangsti");
+    muligeOrd.add("skovsnegl");
+    muligeOrd.add("solsort");
+    nulstil();
+  }
+
+  public void nulstil() {
+    Random random = new Random();
+    brugteBogstaver.clear();
+    antalForkerteBogstaver = 0;
+    spilletErVundet = false;
+    spilletErTabt = false;
+    ordet = muligeOrd.get(random.nextInt(muligeOrd.size()));
+    opdaterSynligtOrd();
+  }
+
+
+  private void opdaterSynligtOrd() {
+    synligtOrd = "";
+    spilletErVundet = true;
+    for (int n=0; n<ordet.length(); n++) {
+      String bogstav = ordet.substring(n, n+1);
+      if (brugteBogstaver.contains(bogstav)) {
+        synligtOrd = synligtOrd + bogstav;
+      } else {
+        synligtOrd = synligtOrd + "*";
+        spilletErVundet = false;
+      }
+    }
+  }
+
+  public void gætBogstav(String bogstav) {
+    if (bogstav.length()!=1) return;
+    System.out.println("Der gættes på bogstavet: "+bogstav);
+    if (brugteBogstaver.contains(bogstav)) return;
+    if (spilletErVundet || spilletErTabt) return;
+
+    brugteBogstaver.add(bogstav);
+
+    if (ordet.contains(bogstav)) {
+      sidsteBogstavVarKorrekt = true;
+      System.out.println("Bogstavet var korrekt: "+bogstav);
+    }  else {
+      // Vi gættede på et bogstav der ikke var i ordet.
+      sidsteBogstavVarKorrekt = false;
+      System.out.println("Bogstavet var IKKE korrekt: "+bogstav);
+      antalForkerteBogstaver = antalForkerteBogstaver + 1;
+      if (antalForkerteBogstaver>6) {
+        spilletErTabt = true;
+      }
+    }
+    opdaterSynligtOrd();
+  }
+
+  public void logStatus() {
+    System.out.println("---------- ");
+    System.out.println("- ordet (skult) = "+ordet);
+    System.out.println("- synligtOrd = "+synligtOrd);
+    System.out.println("- forkerteBogstaver = "+antalForkerteBogstaver);
+    System.out.println("- brugeBogstaver = "+brugteBogstaver);
+    if (spilletErTabt) System.out.println("- SPILLET ER TABT");
+    if (spilletErVundet) System.out.println("- SPILLET ER VUNDET");
+    System.out.println("---------- ");
+  }
+
+
+  public static String læsInputStreamSomStreng(InputStream is) throws IOException {
+    char[] buffer = new char[1024];
+    StringBuilder out = new StringBuilder();
+    Reader in = new InputStreamReader(is, "UTF-8");
+    int read;
+    do {
+      read = in.read(buffer, 0, buffer.length);
+      if (read>0) {
+        out.append(buffer, 0, read);
+      }
+    } while (read>=0);
+    in.close();
+    return out.toString();
+  }
+
+
+  public void hentOrdFraDr() throws IOException {
+    URL url = new URL("http://dr.dk");
+    String data = læsInputStreamSomStreng(url.openStream());
+    System.out.println("data = "+data);
+
+    data = data.replaceAll("<.+?>", " ").toLowerCase().replaceAll("[^a-zæøå]", " ").replaceAll(" [a-zæøå] ", " ");
+    System.out.println("data = "+data);
+    muligeOrd.clear();
+    muligeOrd.addAll(new HashSet<String>(Arrays.asList(data.split(" "))));
+
+    System.out.println("muligeOrd = "+muligeOrd);
+    nulstil();
+  }
+}
